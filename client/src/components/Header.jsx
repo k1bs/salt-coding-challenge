c2886cea8952232ab672092fc4304cc9ba8c6609
Alex Kibler <alexkibler@me.com>
2018-02-22 18:46:38 -0500

    adjust styling of navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,14 +3,16 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Header = (props) => (
-  <nav className='navbar navbar-dark bg-dark'>
-    <h1 className='navbar-brand'>App Title</h1>
-    <ul className='navbar-nav'>
-      <li className='nav-item'><Link className='nav-link' to='/'>Home</Link></li>
-      {!props.auth && <li className='nav-item'><Link className='nav-link' to='/register'>Register</Link></li>}
-      {!props.auth && <li className='nav-item'><Link className='nav-link' to='/login'>Login</Link></li>}
-      {props.auth && <li className='nav-item'><span className='nav-link logout' onClick={props.logout}>Logout</span></li>}
-    </ul>
+  <nav className='navbar navbar-expand navbar-dark bg-dark'>
+    <span className='navbar-brand'>App Title</span>
+    <div className='collapse navbar-collapse' id='navbarNav'>
+      <ul className='nav navbar-nav'>
+        <li className='nav-item'><Link className='nav-link' to='/dashboard'>Dashboard</Link></li>
+        {!props.auth && <li className='nav-item'><Link className='nav-link' to='/register'>Register</Link></li>}
+        {!props.auth && <li className='nav-item'><Link className='nav-link' to='/login'>Login</Link></li>}
+        {props.auth && <li className='nav-item'><span className='nav-link logout' onClick={props.logout}>Logout</span></li>}
+      </ul>
+    </div>
   </nav>
 )
 
