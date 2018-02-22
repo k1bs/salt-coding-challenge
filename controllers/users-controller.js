902b55d126f05e067515190e20b3a062c0a8b115
Alex Kibler <alexkibler@me.com>
2018-02-22 14:52:42 -0500

    update user controller

---
+++ controllers/users-controller.js
@@ -0,0 +1,25 @@
+const bcrypt = require('bcryptjs')
+const User = require('../models/User.js')
+
+const usersController = {}
+
+usersController.create = (req, res, next) => {
+  const salt = bcrypt.genSaltSync()
+  const hash = bcrypt.hashSync(req.body.password, salt)
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password_digest: hash
+  }).then((user) => {
+    req.login(user, (err) => {
+      if (err) return next(err)
+      res.status(201).json({
+        message: 'user created successfully',
+        auth: true,
+        data: {
+          user
+        }
+      })
+    })
+  }).catch(next)
+}
