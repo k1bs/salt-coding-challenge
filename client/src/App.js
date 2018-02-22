ff00b0ebeccd9d737005637372ec6a8324bcdf3f
Alex Kibler <alexkibler@me.com>
2018-02-22 15:25:44 -0500

    add submit methods and call to login stuff to server.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import fetch from 'cross-fetch'
 
-import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Footer from './components/Footer'
 import Home from './components/Home'
 import Header from './components/Header'
+import Login from './components/Login'
+import Register from './components/Register'
 import './App.css'
 
 class App extends Component {
@@ -15,6 +18,44 @@ class App extends Component {
     }
   }
 
+  handleLoginSubmit (e, data) {
+    e.preventDefault()
+    fetch('/api/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include',
+      body: JSON.stringify(data)
+    }).then((res) => res.json())
+      .then((json) => {
+        console.log(json)
+        this.setState({
+          auth: json.auth,
+          user: json.data.user
+        })
+      }).catch((err) => console.log(err))
+  }
+
+  handleRegisterSubmit (e, data) {
+    e.preventDefault()
+    fetch('/api/auth/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include',
+      body: JSON.stringify(data)
+    }).then((res) => res.json())
+      .then((json) => {
+        console.log(json)
+        this.setState({
+          auth: json.auth,
+          user: json.data.user
+        })
+      }).catch((err) => console.log(err))
+  }
+
   render () {
     return (
       <Router>
@@ -22,6 +63,16 @@ class App extends Component {
           <Header />
           <div className='container'>
             <Route exact path='/' component={Home} />
+            <Route exact path='/login' render={() => (
+              this.state.auth
+                ? <Redirect to='/' />
+                : <Login handleLoginSubmit={this.handleLoginSubmit} />
+            )} />
+            <Route exact path='/register' render={() => (
+              this.state.auth
+                ? <Redirect to='/' />
+                : <Register handleRegisterSubmit={this.handleRegisterSubmit} />
+            )} />
           </div>
           <Footer />
         </div>
