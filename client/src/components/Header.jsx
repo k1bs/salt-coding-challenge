413ada7783e8662f38c191c47633b76ab431b8d6
Alex Kibler <alexkibler@me.com>
2018-02-22 16:32:40 -0500

    conditionally render header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 import { Link } from 'react-router-dom'
 
-const Header = () => (
+const Header = (props) => (
   <nav className='navbar navbar-dark bg-dark'>
     <h1 className='navbar-brand'>App Title</h1>
     <ul className='navbar-nav'>
       <li className='nav-item'><Link className='nav-link' to='/'>Home</Link></li>
-      <li className='nav-item'><Link className='nav-link' to='/register'>Register</Link></li>
-      <li className='nav-item'><Link className='nav-link' to='/login'>Login</Link></li>
+      {!props.auth && <li className='nav-item'><Link className='nav-link' to='/register'>Register</Link></li>}
+      {!props.auth && <li className='nav-item'><Link className='nav-link' to='/login'>Login</Link></li>}
+      {props.auth && <li className='nav-item'><span className='nav-link logout' onClick={props.logout}>Logout</span></li>}
     </ul>
   </nav>
 )
