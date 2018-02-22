7b7d0f5c25a435f334717ae8d36164498963de46
Alex Kibler <alexkibler@me.com>
2018-02-22 18:42:27 -0500

    create seed for orders

---
+++ db/seeds/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO orders (ts, from_curr, from_amt, to_curr, to_amt, user_id) VALUES (current_timestamp, 'USD', 200, 'BTC', 0.4567, 1);
+INSERT INTO orders (ts, from_curr, from_amt, to_curr, to_amt, user_id) VALUES (current_timestamp, 'USD', 1000, 'BTC', 1, 1);
+INSERT INTO orders (ts, from_curr, from_amt, to_curr, to_amt, user_id) VALUES (current_timestamp, 'BTC', .4, 'DOGE', 20, 1);
