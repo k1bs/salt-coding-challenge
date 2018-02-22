f3dba1cd35a3eade880bc77cea6e2bf24a500c7e
Alex Kibler <alexkibler@me.com>
2018-02-22 14:11:50 -0500

    change migration

--- db/migrations/migration-1320-21022018.sql
+++ db/migrations/migration-1320-21022018.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS orders (
   id SERIAL PRIMARY KEY,
   ts TIMESTAMP,
   from_curr VARCHAR(255),
-  from_amt DECIMAL(19, 4),
+  from_amt DECIMAL(19, 8),
   to_curr VARCHAR(255),
   to_amt DECIMAL(19, 4),
   user_id INT REFERENCES user(id)
