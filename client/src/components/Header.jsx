bc7f6158a476e14d52c43cea191b9b03fc2e94a6
Alex Kibler <alexkibler@me.com>
2018-02-22 16:03:03 -0500

    remove unncessary shit

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,10 +3,12 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Header = () => (
-  <nav className='navbar'>
+  <nav className='navbar navbar-dark bg-dark'>
     <h1 className='navbar-brand'>App Title</h1>
     <ul className='navbar-nav'>
-      <li classname='nav-item'><Link className='nav-link' to='/'>Home</Link></li>
+      <li className='nav-item'><Link className='nav-link' to='/'>Home</Link></li>
+      <li className='nav-item'><Link className='nav-link' to='/register'>Register</Link></li>
+      <li className='nav-item'><Link className='nav-link' to='/login'>Login</Link></li>
     </ul>
   </nav>
 )
