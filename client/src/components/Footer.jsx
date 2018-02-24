64b3aa96e4ecd4cadca5e6a03765131138e5548e
Alex Kibler <alexkibler@me.com>
2018-02-24 15:42:20 -0500

    add class for styling

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Footer = () => (
-  <footer className='footer'>
+  <footer className='footer footer-container'>
     <div className='container'>
       <span>Alex Kibler for SALT Lending</span><br />
       <span>Crypto prices provided by <a href='https://www.cryptocompare.com/' target='_blank' rel='noopener noreferrer'>CryptoCompare</a></span>
