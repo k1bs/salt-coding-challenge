c6c53b6b1e9b4543664068de5434542db3eb8891
Alex Kibler <alexkibler@me.com>
2018-02-24 15:41:09 -0500

    change app title

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Header = (props) => (
-  <nav className='navbar navbar-expand navbar-dark bg-dark'>
-    <span className='navbar-brand'>App Title</span>
+  <nav className='navbar navbar-expand navbar-dark bg-dark header-container'>
+    <Link to='/' className='navbar-brand'>Crypto 101</Link>
     <div className='collapse navbar-collapse' id='navbarNav'>
       <ul className='nav navbar-nav'>
         <li className='nav-item'><Link className='nav-link' to='/dashboard'>Dashboard</Link></li>
