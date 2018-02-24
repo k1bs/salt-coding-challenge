2db353f17351995b1d2751b68101b4860ec71084
Alex Kibler <alexkibler@me.com>
2018-02-24 15:41:18 -0500

    add more descriptive home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
-const Home = () => (
-  <div>
-    <h1>TEMPORARY HOME PAGE</h1>
-  </div>
-)
+class Home extends Component {
+  render () {
+    console.log(this.props.auth)
+    return (
+      <div>
+        <div className='jumbotron'>
+          <h1 className='display-3'>Crypto 101</h1>
+          <hr className='my-4' />
+          <p>In this class, you will learn the ins and outs of trading with Cryptocurrencies. Each student will be given an imaginary $10,000 to invest. By closely monitoring market prices, a student will be able to grow their virtual wealth. The student with the highest total value by the end of the course will receive a bonus to their final grade!</p>
+          {this.props.auth
+          ? <Link className='btn btn-primary' to='/dashboard'>Dashboard</Link>
+          : <div className='btn-group' >
+            <Link className='btn btn-primary' to='/login'>Login</Link>
+            <Link className='btn btn-secondary' to='/register'>Register</Link>
+          </div>}
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Home
